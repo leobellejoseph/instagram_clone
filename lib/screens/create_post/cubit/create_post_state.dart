95e183b5e1e8b7d31a29d9fdbe2d1be4cee97990
Leobelle Joseph Villocog <leobellejosephvillocog@outlook.com>
2021-05-21 23:13:10 +0800

    profile

---
+++ lib/screens/create_post/cubit/create_post_state.dart
@@ -0,0 +1,10 @@
+part of 'create_post_cubit.dart';
+
+abstract class CreatePostState extends Equatable {
+  const CreatePostState();
+}
+
+class CreatePostInitial extends CreatePostState {
+  @override
+  List<Object> get props => [];
+}
